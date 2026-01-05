d85702386074880a79f0de28806640f41c2148f6
Anthony Piegaro <84252391+anthonypiegaro@users.noreply.github.com>
2026-01-05 18:56:50 +0000

    Add RSVP Response UI

--- app/rsvp.tsx
+++ app/rsvp.tsx
@@ -17,6 +17,7 @@ import {
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
+import { Spinner } from "@/components/ui/spinner"
 import { Textarea } from "@/components/ui/textarea"
 
 export const rsvpSchema = z.object({
@@ -28,6 +29,20 @@ export const rsvpSchema = z.object({
 export type RSVPSchema = z.infer<typeof rsvpSchema>
 
 export function RSVP() {
+  const [submittedResponse, setSubmittedResponse] = useState<null | boolean>(null)
+
+  if (submittedResponse !== null) {
+    return <RSVPSuccess isGoing={submittedResponse} />
+  }
+
+  return <RSVPForm onSuccess={(response) => setSubmittedResponse(response)} />
+}
+
+function RSVPForm({ 
+  onSuccess
+}: {
+  onSuccess: (response: boolean) => void
+}) {
   const [isSubmitting, setIsSubmitting] = useState(false)
 
   const form = useForm<RSVPSchema>({
@@ -38,6 +53,16 @@ export function RSVP() {
       message: ""
     }
   })
+
+  const onSubmit = async (values: RSVPSchema) => {
+    setIsSubmitting(true)
+
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+
+    onSuccess(values.attending === "Yes" ? true : false)
+
+    setIsSubmitting(false)
+  }
 
   return (
     <section id="rsvp" className="mt-25 px-5">
@@ -57,7 +82,11 @@ export function RSVP() {
                 <FormItem>
                   <FormLabel>Name</FormLabel>
                   <FormControl>
-                    <Input {...field} disabled={isSubmitting} className="max-w-sm border-2 border-black/20" />
+                    <Input 
+                      {...field} 
+                      disabled={isSubmitting}
+                      autoComplete="off"
+                      className="max-w-sm border-2 border-black/20" />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -101,16 +130,39 @@ export function RSVP() {
                       className="border-2 border-black/20 w-full max-w-full min-w-0 h-50 resize-none mb-2 whitespace-pre-wrap break-words"
                       {...field}
                       disabled={isSubmitting}
+                      autoComplete="off"
                     />
                   </FormControl>
                 </FormItem>
               )}
             />
           </form>
-          <Button type="button" className="mt-2 cursor-pointer">
-              Submit RSVP
+          <Button 
+            type="button" 
+            className="mt-2 cursor-pointer" 
+            disabled={isSubmitting}
+            onClick={form.handleSubmit(onSubmit)}
+          >
+              {isSubmitting ? <><Spinner /> Submitting...</> : "Submit RSVP"}
           </Button>
         </Form>
+      </div>
+    </section>
+  )
+}
+
+function RSVPSuccess({
+  isGoing
+}: {
+  isGoing: boolean
+}) {
+
+  const yesMessage = "Thank you for coming to celebrate our growing family. We’re so excited to see you there!"
+  const noMessage = "Thank you for letting us know. We’ll miss you but truly appreciate your love and support!"
+  return (
+    <section id="rsvp" className="mt-25 px-5">
+      <div className="h-124 w-full flex justify-center items-center text-center text-lg font-semibold [font-family:var(--font-raleway)] max-w-xl mx-auto bg-pink-300/20 backdrop-blur-sm rounded-xl py-5 px-5">
+        <p className="max-w-sm">{isGoing ? yesMessage : noMessage}</p>
       </div>
     </section>
   )
